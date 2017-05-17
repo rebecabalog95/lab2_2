e2c1e201f42e3a380a7be090cac873811e52ade4
Rebeca <rebecabalog95@gmail.com>
2017-05-17 09:24:32 +0300

    e

--- src/test/java/repository/StoreRepositoryTest.java
+++ src/test/java/repository/StoreRepositoryTest.java
@@ -40,9 +40,10 @@ public class StoreRepositoryTest {
 
     @Test
     public void addNewProduct() throws Exception {
+
+        assertEquals("eroare in addNewProduct",0,repository.getAllProducts().size());
         //modificare
-        //assertEquals("eroare in addNewProduct",0,repository.getAllProducts().size());
-        assertEquals("eroare in addNewProduct",1,repository.getAllProducts().size());
+        //assertEquals("eroare in addNewProduct",1,repository.getAllProducts().size());
         repository.addNewProduct(product1);
         assertEquals("eroare in addNewProduct",product1,repository.getAllProducts().get(0));
         repository.addNewProduct(product2);
